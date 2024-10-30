28538ceeeffd26569553b6d19607cb40b34aa1de
manualdiaz <ananta.ghosh@teamfuture.in>
2024-10-30 12:33:48 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showContactInfo() {
+    const contactInfo = document.getElementById("contact-info");
+    if (contactInfo.style.display === "none") {
+        contactInfo.style.display = "block";
+    } else {
+        contactInfo.style.display = "none";
+    }
+}
